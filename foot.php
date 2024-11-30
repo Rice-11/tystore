5c8a8ef7cd2fb065942df5e4e50819fca7554004
Ehud <tan.ehud@gmail.com>
2024-11-30 17:05:34 +0800

    first interation?

---
+++ foot.php
@@ -0,0 +1,12 @@
+</main>
+    <link rel="stylesheet" href="css/footer.css">
+<footer>
+   <!-- <div> Developed by <b>Ehud</b>  &middot; </div>
+    Copyrighted &copy; <?= date('Y') ?> -->
+    
+    <div class="container">
+        
+    </div>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is a title</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <head>This is an assigment</head>
+
+    <section>
+        <p> THIS IS A LONG AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHH</p>
+    </section>
+    <section>
+        <p> THIS IS A LONG AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHH</p>
+    </section>
+    <section>
+        <p> THIS IS A LONG AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHH</p>
+    </section>
+</body>
+
+<footer>
+    <?php 
+    include("foot.php"); ?>
+
+</footer>
+</html>
